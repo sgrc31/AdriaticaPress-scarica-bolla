20131b84a208282de88ad692953ec98509efd366
Simone Guercio <s.guercio@gmail.com>
2016-06-26 01:55:34 +0200

    Added code to write output to file

--- apdatadump.py
+++ apdatadump.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import time
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
+start_time = time.time()
 driver = webdriver.Chrome()
 driver.get('http://www.adriaticapress.com/Login.htm')
 
@@ -21,7 +22,7 @@ username_field.send_keys(username)
 password_field.clear()
 password_field.send_keys(password)
 password_field.send_keys(Keys.RETURN)
-
+time.sleep(5)
 #Pagina bolla di prova, da modificare TODO
 driver.get('http://www.adriaticapress.com/Bolla.htm-24/06/2016-B')
 time.sleep(20) #Il caricamendo delle bolle è spesso lento TODO
@@ -36,10 +37,21 @@ for link in elenco_link_testate:
     dati_testata = []
     driver.execute_script('arguments[0].scrollIntoView();', link)
     link.click()
-    time.sleep(2)
+    time.sleep(3)
     nome_testata = driver.find_element_by_id('lblTitoloDettaglio').text
     identificativo_testata = driver.find_element_by_id('lblCodice').text
     numero_testata = driver.find_element_by_id('lblNumeroDettaglio').text
     barcode_testata = driver.find_element_by_id('lblBarcodeDettaglio').text
     dati_testata.extend([nome_testata, identificativo_testata, numero_testata, barcode_testata])
     lista_bolla.append(dati_testata)
+
+#Scrivo l'output su .csv
+output_file = open('bolla.csv', 'w')
+output_writer = csv.writer(output_file)
+output_writer.writerow(['Testata', 'Pubblicazione', 'Numero', 'Barcode'])
+for row in lista_bolla:
+    output_writer.writerow(row)
+output_file.close()
+driver.close()
+total_time = time.time() - start_time
+print('Dump completato in ' + str(round(total_time)) + ' secondi.')
