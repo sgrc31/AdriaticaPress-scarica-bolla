d2aaa5594c2db7a6f0187b8a2ab26ea8b6ce0aa8
Simone Guercio <s.guercio@gmail.com>
2016-06-27 17:53:56 +0200

    Non ho la minima idea di perché abbia fatto questo commit

--- apdatadump.py
+++ apdatadump.py
@@ -25,34 +25,32 @@ start_time = time.time()
 driver = webdriver.Chrome()
 driver.get('http://www.adriaticapress.com/Login.htm')
 
-time.sleep(15)
+time.sleep(5)
 
 #Login credentials
-username_field = driver.find_element_by_id('txtUsername')
-password_field = driver.find_element_by_id('txtPassword')
+username_field = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'txtUsername')))
+password_field = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'txtPassword')))
 username_field.clear()
 username_field.send_keys(username)
 password_field.clear()
 password_field.send_keys(password)
 password_field.send_keys(Keys.RETURN)
-time.sleep(5)
+time.sleep(3)
 #Pagina bolla di prova, da modificare TODO
 driver.get('http://www.adriaticapress.com/Bolla.htm-24/06/2016-B')
-time.sleep(20) #Il caricamendo delle bolle è spesso lento TODO
 
-#Rendo l'header statico, così da non creare problemi con lo scroll
+#Le pagine delle bolle necessitano di abbastanza tempo per caricarsi
+time.sleep(20)
+
+#Rendo header statico, così da non intralciarmi nello scroll sui link
 driver.execute_script('$(".superHeader").css({position: "static"});')
-
 lista_bolla = []
-elenco_link_testate = driver.find_elements_by_css_selector('a[id^=\"ctl00_MainContent_dlBolla_ctl\"]')
-
+elenco_link_testate = WebDriverWait(driver, 50).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[id^=\"ctl00_MainContent_dlBolla_ctl\"]')))
 for link in elenco_link_testate:
     dati_testata = []
     driver.execute_script('arguments[0].scrollIntoView();', link)
     link.click()
-    #time.sleep(3)
-    #nome_testata = driver.find_element_by_id('lblTitoloDettaglio').text
-    nome_testata = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, 'lblTitoloDettaglio')))
+    nome_testata = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, 'lblTitoloDettaglio'))).text
     identificativo_testata = driver.find_element_by_id('lblCodice').text
     numero_testata = driver.find_element_by_id('lblNumeroDettaglio').text
     barcode_testata = driver.find_element_by_id('lblBarcodeDettaglio').text
