b81d9622a705c9ae727c3b362bd395f6fc90e300
Simone Guercio <s.guercio@gmail.com>
2016-08-18 23:28:50 +0200

    Add proper init to the dialog

--- scarico_bolla.py
+++ scarico_bolla.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QCalendarWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QCalendarWidget, QHBoxLayout, QVBoxLayout, QDialog
 
 
-class MyWin(QWidget):
+class MyWin(QDialog):
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super(MyWin, self).__init__(parent)
         
         self.initUI()
         
         
     def initUI(self):
         #Creo calendario
-        calendar_label = QLabel('Seleziona la data', self)
-        calendar = QCalendarWidget(self)
+        self.calendar_label = QLabel('Seleziona la data', self)
+        self.calendar = QCalendarWidget(self)
         #Creo i bottoni
-        btn_download = QPushButton('Scarica Bolle', self)
-        btn_upload = QPushButton('Carica Vendite', self)
+        self.btn_download = QPushButton('Scarica Bolle', self)
+        self.btn_upload = QPushButton('Carica Vendite', self)
         #Creo layout
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
-        hbox.addWidget(btn_download)
-        hbox.addWidget(btn_upload)
-        vbox.addWidget(calendar_label)
-        vbox.addWidget(calendar)
+        hbox.addWidget(self.btn_download)
+        hbox.addWidget(self.btn_upload)
+        vbox.addWidget(self.calendar_label)
+        vbox.addWidget(self.calendar)
         vbox.addLayout(hbox)
         self.setLayout(vbox)
+        self.setWindowTitle('Occupy Artoni')
         self.show()
 
 if __name__ == '__main__':
