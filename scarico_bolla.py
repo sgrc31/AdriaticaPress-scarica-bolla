12e3044ecaf00565e3eafd59d709b9e4d708a38a
Simone Guercio <s.guercio@gmail.com>
2016-08-19 13:10:53 +0200

    Add basic button signal to funcion and return output in widget

--- scarico_bolla.py
+++ scarico_bolla.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QCalendarWidget, QHBoxLayout, QVBoxLayout, QDialog
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QCalendarWidget, QHBoxLayout, QVBoxLayout, QDialog, QTextEdit
+#from PyQt5.QtCore import pyqtSlot
 
 
 class MyWin(QDialog):
@@ -19,6 +20,10 @@ class MyWin(QDialog):
         #Creo i bottoni
         self.btn_download = QPushButton('Scarica Bolle', self)
         self.btn_upload = QPushButton('Carica Vendite', self)
+        #Creo widget testo per output
+        self.mio_testo = QTextEdit(self)
+        self.mio_testo.setReadOnly(True)
+        self.mio_testo.setMaximumHeight(50)
         #Creo layout
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
@@ -27,12 +32,17 @@ class MyWin(QDialog):
         vbox.addWidget(self.calendar_label)
         vbox.addWidget(self.calendar)
         vbox.addLayout(hbox)
+        vbox.addWidget(self.mio_testo)
         self.setLayout(vbox)
+ 
+        def test_funct():
+            self.mio_testo.setPlainText('Test text 123')
+
+        self.btn_download.clicked.connect(test_funct)
         self.setWindowTitle('Occupy Artoni')
         self.show()
 
 if __name__ == '__main__':
-    
     app = QApplication(sys.argv)
     ex = MyWin()
     sys.exit(app.exec_())
